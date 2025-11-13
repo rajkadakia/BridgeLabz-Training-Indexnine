ea5860e5a8c97638432d58e426b8d8ed2097f6ca
rajKadakia <rajkadakia7@gmail.com>
2025-11-13 15:09:23 +0530

    completed sinle linkedlist

--- DSA/Linkedlist/linklist.js
+++ DSA/Linkedlist/linklist.js
@@ -58,6 +58,49 @@ class linkedlist{
         n.next=current.next;
         current.next=n;
     }
+
+    removetop(){
+        if(!this.head){
+            return;
+        }
+        this.head=this.head.next;
+    }
+
+    removelast(){
+        if(!this.head)
+            return;
+    let current = this.head;
+    while(current.next.next){
+        current=current.next
+    }
+    current.next=null;
+    }
+    removeatindex(index){
+        if(index<0||index>this.size()){
+            console.error("invalid index");
+            return;
+        }
+        if(index===0){
+            this.head=this.head.next;
+        }
+        let current = this.head
+        for(let i = 0;i<index-1;i++){
+            current = current.next;
+        }
+        if(current.next)
+        current.next=current.next.next;
+
+    }
+
+    print(){
+        let current = this.head;
+        while(current){
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+        
+
 }
 const ll = new linkedlist;
 ll.addfirst(200);
@@ -69,5 +112,11 @@ ll.addatindex(3,400);
 ll.addatindex(5,700);
 ll.addatindex(3,600);
 ll.addatindex(0,300);
-
-console.log(ll.size());
+ll.removetop();
+ll.removetop();
+ll.addatindex(3,500);
+ll.addatindex(5,900);
+ll.removelast();
+ll.removetop();
+ll.removeatindex(3);
+ll.print();
